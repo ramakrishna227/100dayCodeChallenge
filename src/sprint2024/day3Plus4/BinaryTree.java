e0eea9a7583f3db1d48424e0503d4d827d967b44
ramakrishna <ramakrishna227@gmail.com>
2024-01-20 23:25:25 +0530

    day3

---
+++ src/sprint2024/day3Plus4/BinaryTree.java
@@ -0,0 +1,50 @@
+package sprint2024.day3Plus4;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public int height(Node root) {
+        if (root == null)
+            return 0;
+        else {
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+            if (lheight > rheight)
+                return lheight + 1;
+            else
+                return rheight + 1;
+        }
+    }
+
+    public void printCurrentLevel(Node root, int level) {
+        if (root == null)
+            return;
+        if (level == 1)
+            System.out.print(root.data + " ");
+        else if (level > 1) {
+            printCurrentLevel(root.left, level - 1);
+            printCurrentLevel(root.right, level - 1);
+        }
+    }
+    void printLevelOrder(){
+        int h=height(root);
+        for(int i=1; i<=h; i++)
+            printCurrentLevel(root, i);
+    }
+
+    public static void main(String[] args){
+        BinaryTree tree=new BinaryTree();
+        tree.root=new Node(1);
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+        tree.root.left.left=new Node(4);
+        tree.root.left.right=new Node(5);
+
+        System.out.println("level order traversal of above binary tree is ");
+        tree.printLevelOrder();
+    }
+}
---
+++ src/sprint2024/day3Plus4/Node.java
@@ -0,0 +1,12 @@
+package sprint2024.day3Plus4;
+
+//Node class for binary trees
+public class Node {
+    int data;
+    Node left, right;
+
+    public Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
