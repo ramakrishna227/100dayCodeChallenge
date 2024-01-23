55bc9127accd8d31e8c9a4d5758ee584114a2b78
ramakrishna <ramakrishna227@gmail.com>
2024-01-23 22:21:35 +0530

    day7

---
+++ src/sprint2024/day7/DFSGraph.java
@@ -0,0 +1,46 @@
+package sprint2024.day7;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DFSGraph {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    DFSGraph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v + " ");
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    public static void main(String[] args) {
+        DFSGraph g = new DFSGraph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+        System.out.println("DFS following verted 2 is ");
+
+        boolean visited[] = new boolean[g.V];
+        g.DFSUtil(2, visited);
+    }
+}
