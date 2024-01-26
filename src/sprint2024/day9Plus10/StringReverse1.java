6fe154d5563f88535a10088baa9f761b053d10f8
ramakrishna <ramakrishna227@gmail.com>
2024-01-26 13:26:19 +0530

    string reversal

---
+++ src/sprint2024/day9Plus10/StringReverse1.java
@@ -0,0 +1,33 @@
+package sprint2024.day9Plus10;
+
+public class StringReverse1 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        StringReverse1.reverseUsingStringBuilder(str);
+        StringReverse1.reverseUsingCharArray(str);
+        StringReverse1.reverseUsingCharAt(str);
+    }
+
+    public static void reverseUsingStringBuilder(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("reverse of \"" + str + "\" is \"" + sb.reverse() + "\"");
+    }
+
+    public static void reverseUsingCharArray(String str) {
+        char[] charArray = str.toCharArray();
+        String reversed = "";
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            reversed = reversed + charArray[i];
+        }
+        System.out.println("reverse of \"" + str + "\" is \"" + reversed + "\"");
+    }
+
+    public static void reverseUsingCharAt(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+        System.out.println("reverse of \"" + str + "\" is \"" + reversed + "\"");
+    }
+
+}
